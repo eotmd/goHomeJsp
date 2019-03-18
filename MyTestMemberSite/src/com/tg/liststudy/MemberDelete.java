0b892837886cd64679c632a9f2b9302d826dc7c8
eotmd <eotmd549@naver.com>
2019-03-18 10:17:41 +0900

    작성자: 박대승
    내용
    딜리트에서 post삭제

--- MyTestMemberSite/src/com/tg/liststudy/MemberDelete.java
+++ MyTestMemberSite/src/com/tg/liststudy/MemberDelete.java
@@ -87,65 +87,6 @@ public class MemberDelete extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-		System.out.println("MemberUpdateServlet의 doPost를 탄다.");
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "jsp";
-		String password = "jsp";
-
-		req.setCharacterEncoding("UTF-8");
-
-		String emailStr = req.getParameter("email");
-		int mno = Integer.parseInt(req.getParameter("mNo"));
-		String nameStr = req.getParameter("name");
-		String pwdStr = req.getParameter("pwd");
-
-		String sql = "";
-
-		try {
-
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection(url, user, password);
-
-			sql = "UPDATE MEMBERS";
-			sql += " SET EMAIL = ?, MNAME = ?, MOD_DATE = SYSDATE";
-			sql += " WHERE MNO = ?";
-			
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, emailStr);
-			pstmt.setString(2, nameStr);
-			pstmt.setInt(3, mno);
-
-			pstmt.executeUpdate(); // 수정이 이루워졌을때 사용
-
-			res.sendRedirect("./one?email="+emailStr+"&password="+pwdStr);
-
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		} // finally end
 
 	}
 
